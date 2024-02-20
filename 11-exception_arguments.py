8a27cbcaaa043c1f1331fb76c13ecfe7f43738eb
Samuel Odumu <themainsamuel@gmail.com>
2024-02-20 05:08:54 +0100

    more exceptions

--- 11-exception_arguments.py
+++ 11-exception_arguments.py
@@ -1,3 +1,12 @@
 #!/usr/bin/bash
 
+def div(a, b):
+    result = 0
+    try:
+        result = a / float(b)
+    except ZeroDivisionError:
+        print("Error: Division by zero is not allowed.")
+    return result
 
+print(div(10, 2)) # Should print 5.0
+print(div(10, 0)) # Should print an error message
