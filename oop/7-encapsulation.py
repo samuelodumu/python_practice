a4d73c191e85460e2560f708556163caffe25fed
Samuel <themainsamuel@gmail.com>
2024-04-08 22:39:34 +0100

    understanding the difference between public, private and protected attributes

--- oop/7-encapsulation.py
+++ oop/7-encapsulation.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
 
 class Robot:
-    def __init__(self, name=None, build=None):
-        self.name = name
-        self.build = build
+    def __init__(self, name=None, build_year=2000):
+        self.__name = name
+        self.__build = build_year
     def say_hi(self):
-        if self.name:
-            print(f"Hi, my name is {self.name}")
+        if self.__name:
+            print(f"Hi, my name is {self.__name}")
         else:
             print("Hi, I'm without a name for now")
-        if self.build:
-            print(f"I was made in {self.build}")
-        else:
-            print("My build year is not currently available")
 
     def set_name(self, name):
-        self.name = name
+        self.__name = name
     def get_name(self):
-        return self.name
+        return self.__name
 
     def set_build(self, b_y):
-        self.build = b_y
+        self.__build = b_y
     def get_build(self):
-        return self.build
-x = Robot()
-x.say_hi()
-x.set_name('Matt')
-x.set_build(1930)
-x.say_hi()
+        return self.__build
+
+if __name__ == "__main__":
+    x = Robot("Milan", 1940)
+    y = Robot("Casper", 2003)
+    for a in [x, y]:
+        a.say_hi()
+        if a.get_name() == "Casper":
+            a.set_build(2005)
+        print(f"I was made in the year {a.get_build()}!")
---
+++ oop/attribute_tests.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class A:
+    def __init__(self):
+        self.pub = "I am public"
+        self._prot = "I am protected"
+        self.__priv = "I am private"
