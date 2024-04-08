a16aaa9dacd387e22c202a9e9884b9abd998c43b
Samuel <themainsamuel@gmail.com>
2024-04-08 12:50:32 +0100

    renamed a few files and the 'classes' folder to 'oop'

---
+++ oop/0-simplest_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class Person:
+    pass
+
+p = Person()
+print(p)
---
+++ oop/8-str_repr.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import datetime
+today = datetime.datetime.now()
+print(str(today))
+print(repr(today))
+time = repr(today)
+
+print(eval(time) == today)
