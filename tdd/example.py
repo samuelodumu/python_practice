5d496e35e1798906c52f8b5bf5f39981641e0e6f
Samuel <themainsamuel@gmail.com>
2024-04-12 14:20:34 +0100

    trying to understand doctests

---
+++ tdd/example.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""Module that supplies one function, factorial
+>>> factorial(6)
+720
+"""
+
+def factorial(n):
+    """Returns the factorial of a n, an exact integer >= 0
+    >>> [factorial(x) for x in range(6)]
+    [1, 1, 2, 6, 24, 120]
+
+    >>> factorial(-2)
+    Taceback (most recent call last):
+        ...
+    ValueError: n must be >= 0
+    
+    Floats are ok, but the float must be an exact integer:
+    >>> factorial(5.1)
+    Taceback (most recent call last):
+        ...
+    ValueError: n must be an exact integer
+    
+    >>> factorial(1e100)
+    Traceback (most recent call last):
+        ...
+    OverflowError: n is too large
+    """
+    import math
+    if not n >= 0:
+        raise ValueError("n must be >= 0")
+    if math.floor(n) != n:
+        raise ValueError("n must be an exact integer")
+    if n == n + 1:
+        raise OverflowError("n is too large")
+    result = 1
+    factor = 2
+
+    while factor <= n:
+        result *= factor
+        factor += 1
+    return result
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
